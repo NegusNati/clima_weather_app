f2ffa08ea9f8ea749d1500bc097d115ff5fbd2b3
NATNAEL BIRHANU <negusnati@gmail.com>
2022-07-26 01:24:40 -0700

    current location

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -7,13 +7,32 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  void getLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-void getLocation() async {
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled');
+    }
 
-   Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-   print('position');
-}
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
 
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
 
   @override
   Widget build(BuildContext context) {
